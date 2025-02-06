a1f027ce2e6122628919823e86e04ccf3f87ad6b
rahmat-cell <rahmatart343@gmail.com>
2025-02-06 15:21:29 +0700

    Perubahan pada index.html

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden')
 });
+
+document.getElementById("whatsappForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name		= document.getElementById("name").value;
+    var email 		= document.getElementById("email").value;
+    var message 	= document.getElementById("message").value;
+    let contact 	= 'nomor WA kamu'; // isi dengan nomor WA (+6283811223344)
+
+    var encodedMessage = encodeURIComponent(
+        "Nama Depan	: " + name + "\n" +
+        "Email		: " + email + "\n" +
+        "Pesan		: " + message
+    );
+
+    var link;
+
+    // Check if user is on a mobile device
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        link = `whatsapp://send?phone=${contact}&text=${encodedMessage}`;
+    } else { // Desktop device
+        link = `https://api.whatsapp.com/send?phone=${contact}&text=${encodedMessage}`;
+    }
+
+    window.open(link, '_blank');
+});
